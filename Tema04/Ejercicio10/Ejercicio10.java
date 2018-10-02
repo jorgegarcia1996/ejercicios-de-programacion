e7db8fa9168375b663b730950fffa7b56233c7e4
Jorge García Molina <jorgetrapiche1996@gmail.com>
2018-10-02 21:05:14 +0200

    Ejercicio 10 comenzado pero no terminado

---
+++ Tema04/Ejercicio10/Ejercicio10.java
@@ -0,0 +1,20 @@
+/**
+* Ejercicio 10 del Tema 4
+* 
+* Programa que dice el horóscopo a partir del día y el mes de nacimiento
+* 
+* @author Jorge García Molina
+*/
+
+public class Ejercicio10 {
+  public static void main(String[] args) {
+    //Pedir el día y el mes de nacimiento
+    System.out.println("Averigua que horóscopo tienes.");
+    System.out.print("Introduzca su mes de nacimiento: ");
+    double mesNacimiento = Double.parseDouble(System.console().readLine());
+    System.out.print("Introduzca dia de nacimiento: ");
+    double diaNacimiento = Double.parseDouble(System.console().readLine());
+    
+    //Seleccionar el horóscopo
+  }
+}
