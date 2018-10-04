94867075a4f7a5d659f64a9104a0c5effed0d1f5
jorgegarcia1996 <jorgetrapiche1996@gmail.com>
2018-10-04 10:17:52 +0200

    Ejercicio 17 comenzado

---
+++ Tema04/Ejercicio17/Ejercicio17.java
@@ -0,0 +1,20 @@
+/**
+* Ejercicio 17 del Tema 4
+* 
+* Escribe un programa que diga cuál es la última cifra de un número entero
+* introducido por teclado.
+* 
+* @author Jorge García Molina
+*/
+
+public class Ejercicio17 {
+  public static void main(String[] args) {
+    //Pedir un número entero
+    System.out.println("Introduce un número entero y separa la última cifra.");
+    System.out.print("Introduce el número entero: ");
+    int numero = Integer.parseInt(System.console().readLine());
+    
+    //Seperar la última cifra del número
+    
+  }
+}
