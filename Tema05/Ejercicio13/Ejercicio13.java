14ad01c7409d8e06125e59b18d542356ff4cedf7
jorgegarcia1996 <jorgetrapiche1996@gmail.com>
2018-10-10 09:29:55 +0200

    Ejercicio 13 realizado

--- Tema05/Ejercicio13/Ejercicio13.java
+++ Tema05/Ejercicio13/Ejercicio13.java
@@ -1,38 +1,29 @@
 /**
-* Ejercicio 12 del Tema 5
+* Ejercicio 13 del Tema 5
 * 
-* Escribe un programa que muestre los n primeros términos de la serie de
-* Fibonacci. El primer término de la serie de Fibonacci es 0, el segundo es 1
-* y el resto se calcula sumando los dos anteriores, por lo que tendríamos que
-* los términos son 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144... El número
-* n se debe introducir por teclado.
+* Escribe un programa que lea una lista de diez números y determine cuántos
+* son positivos, y cuántos son negativos.
 * 
 * @author Jorge García Molina
 */
 
-public class Ejercicio12 {
+public class Ejercicio13 {
   public static void main(String[] args) {
-    System.out.println("Cadena de Fibonacci de 'x' números.");
+    System.out.println("Lector de números.");
     //Pedir el número
-    System.out.println("¿Cuántos números desea ver de la cadena fibonacci (minimo 1)?");
-    int cantidad = Integer.parseInt(System.console().readLine());
+    System.out.println("Introduzca 10 números enteros, tras cada número debe pulsar intro:");
     
-    //Calcular y mostrar la cadena de fibonacci
-    System.out.println("Los primeros " + cantidad + " numeros de lacadena de fibonacci son: ");
-    int primero = 0;
-    int segundo = 1;
-    int tercero = 0;
-    if (cantidad > 2) {
-      for (int i = 0; i < cantidad; i++) {
-        System.out.print(primero + " ");
-        tercero = primero + segundo;
-        primero = segundo;
-        segundo = tercero;
+    //Leer los números y contar cuantos son de cada tipo
+    int numPos = 0, numNeg = 0, cantidad;
+    for (int i = 0; i < 10; i++) {
+      cantidad = Integer.parseInt(System.console().readLine());
+      if (cantidad < 0) {
+        numNeg++;
+      } else {
+        numPos++;
       }
-    } else if (cantidad == 2) {
-      System.out.print(primero + " " + segundo);
-    } else {
-      System.out.print(primero);
     }
+    System.out.println("Ha introducido " + numPos + " números positivos.");
+    System.out.println("Ha introducido " + numNeg + " números negativos.");
   }
 }
