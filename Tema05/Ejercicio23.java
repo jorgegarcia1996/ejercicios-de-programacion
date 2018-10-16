0ab929a713fd3adc1e917abc7fa8a4c86e6d0249
jorgegarcia1996 <jorgetrapiche1996@gmail.com>
2018-10-16 13:22:39 +0200

    Ejercicio 23 realizado

--- Tema05/Ejercicio23.java
+++ Tema05/Ejercicio23.java
@@ -15,14 +15,23 @@ import java.util.Scanner;
 public class Ejercicio23 { 
   public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
-    System.out.println("Numeros hasta 10000.");
+    System.out.println("Numeros hasta 10.000.");
     //Pedir el primer número
     System.out.print("Introduzca un número entero:");
     int numero = Integer.parseInt(s.nextLine());
     
     //Calcular los números y pedir el siguiente
-    int sumaNumeros = 0, numeroNumeros = 0;
-    double media = 0;
-    
+    int sumaNumeros = numero, nNumeros = 1;
+    double media;
+    while (sumaNumeros < 10000) {
+    System.out.print("Introduzca un número entero:");
+    numero = Integer.parseInt(s.nextLine());
+      sumaNumeros += numero;
+      nNumeros++;
+    }
+    media = sumaNumeros / nNumeros;
+    System.out.println("El total de los números introducidos es: " + sumaNumeros);
+    System.out.println("Se han introducido " + nNumeros + " números.");
+    System.out.printf("La media de los números introducidos es: %.2f", media);
   }
 }
