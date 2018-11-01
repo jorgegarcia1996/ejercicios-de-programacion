ef66f7d4f6e33f13cb8f2f570b917e65f8ceb715
Jorge García Molina <43571181+jorgegarcia1996@users.noreply.github.com>
2018-11-01 19:50:45 +0100

    Ejercicio 14 comenzado

---
+++ Tema06/Ejercicio14.java
@@ -0,0 +1,20 @@
+package tema06;
+/**
+ * Ejercicio 14 del Tema 06
+ * 
+ * Realiza un programa que haga justo lo contrario a lo que hace el ejercicio 6.
+ * El programa intentará adivinar el número que estás pensando - un número entre
+ * 0 y 100 - teniendo para ello 5 oportunidades. En cada intento fallido, el
+ * programa debe preguntar si el número que estás pensando es mayor o menor que
+ * el que te acaba de decir.
+ * 
+ * @author Jorge García Molina
+ */
+public class Ejercicio14 {
+   public static void main(String[] args) {
+    System.out.println("Adivina el número versión 2 (El programa es quién adivina).");
+    
+    //Generar los caracteres y mostrarlos por pantalla
+   
+  }
+}
