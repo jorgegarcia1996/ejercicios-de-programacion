ffadf81cf0ceec51459574b6728623ad963a2a7b
Jorge García Molina <43571181+jorgegarcia1996@users.noreply.github.com>
2018-11-01 18:35:49 +0100

    Ejercicio 11 Realizado

--- Tema06/Ejercicio11.java
+++ Tema06/Ejercicio11.java
@@ -15,8 +15,9 @@ public class Ejercicio11 {
     
     //Generar las notas y contar los tipos de notas
     int suspenso = 0, suficiente = 0, bien = 0, notable = 0, sobresaliente = 0;
-    for (int i = 0; i < 20; i++) {
+    for (int i = 1; i <= 20; i++) {
      int nota = (int)(Math.random() * 11);
+      System.out.printf("%2d. ", i);
      switch (nota) {
        case 0:
        case 1:
@@ -25,7 +26,32 @@ public class Ejercicio11 {
        case 4:
          System.out.println("Suspenso");
          suspenso++;
+         break;
+       case 5:
+         System.out.println("Suficiente");
+         suficiente++;
+         break;
+       case 6:
+         System.out.println("Bien");
+         bien++;
+         break;
+       case 7:
+       case 8:
+         System.out.println("Notable");
+         notable++;
+         break;
+       case 9:
+       case 10:
+         System.out.println("Sobresaliente");
+         sobresaliente++;
+         break;
+       default:
      }
     }
+    System.out.println("Número de Suspensos: " + suspenso);
+    System.out.println("Número de Suficientes: " + suficiente);
+    System.out.println("Número de Bienes: " + bien);
+    System.out.println("Número de Notables: " + notable);
+    System.out.println("Número de Sobresalientes: " + sobresaliente);
   }
 }
