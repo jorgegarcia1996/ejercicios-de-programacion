848fa3100869575aa9169d7b0245e148562e2b4e
Jorge García Molina <43571181+jorgegarcia1996@users.noreply.github.com>
2018-11-07 18:39:05 +0100

    Ejercicio 05 comenzado

---
+++ Tema07/Ejercicio05.java
@@ -0,0 +1,24 @@
+package tema07;
+import java.util.Scanner;
+
+/**
+ * Ejercicio 05 del Tema 07
+ *
+ * Escribe un programa que pida 10 números por teclado y que luego muestre los
+ * números introducidos junto con las palabras “máximo” y “mínimo” al lado del
+ * máximo y del mínimo respectivamente.
+ *
+ * @author Jorge García Molina
+ */
+public class Ejercicio05 {
+
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    System.out.println("Detectar máximo y mínimo de un array con datos"
+            + " introducidos por teclado");
+    System.out.println();
+
+    //Crear el array con los datos del usuario
+    
+  }
+}
