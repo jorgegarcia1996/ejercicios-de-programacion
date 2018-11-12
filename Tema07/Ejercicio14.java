720d1d3230c935fc906ce81dfc688ca25f28e4d6
Jorge García Molina <43571181+jorgegarcia1996@users.noreply.github.com>
2018-11-12 20:12:26 +0100

    Ejercicio 14 comenzado

---
+++ Tema07/Ejercicio14.java
@@ -0,0 +1,27 @@
+package tema07;
+
+import java.util.Scanner;
+
+/**
+ * Ejercicio 14 del Tema 07
+ *
+ * Escribe un programa que pida 8 palabras y las almacene en un array. A
+ * continuación, las palabras correspondientes a colores se deben almacenar al
+ * comienzo y las que no son colores a continuación. Puedes utilizar tantos
+ * arrays auxiliares como quieras. Los colores que conoce el programa deben
+ * estar en otro array y son los siguientes: verde, rojo, azul, amarillo,
+ * naranja, rosa, negro, blanco y morado.
+ *
+ * @author Jorge García Molina
+ */
+public class Ejercicio14 {
+
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    System.out.println("Ordenar palabras de un array");
+    System.out.println();
+
+    //Crear el array y mostrarlo
+    
+  }
+}
