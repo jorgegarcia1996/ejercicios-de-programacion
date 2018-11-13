f432928450e394db8b460c432b05d9150c871e2f
Jorge García Molina <43571181+jorgegarcia1996@users.noreply.github.com>
2018-11-13 10:19:36 +0100

    Ejercicio 14 realizado

--- Tema07/Ejercicio14.java
+++ Tema07/Ejercicio14.java
@@ -21,7 +21,64 @@ public class Ejercicio14 {
     System.out.println("Ordenar palabras de un array");
     System.out.println();
 
-    //Crear el array y mostrarlo
-    
+    //Crear el array con las palabras del usuario y mostrarlas
+    final int N = 8;
+    String[] palabrasDesordenadas = new String[N];
+    for (int i = 0; i < N; i++) {
+      System.out.print("Introduzca una palabra, luego pulse 'INTRO': ");
+      palabrasDesordenadas[i] = s.nextLine().toLowerCase();
+    }
+    System.out.println();
+    System.out.println("-----Palabras introducidas-----");
+    System.out.print("Índices: ");
+    for (int i = 0; i < N; i++) {
+      System.out.printf("%15d |", i);
+    }
+    System.out.println();
+    System.out.print("Valores: ");
+    for (String elemento : palabrasDesordenadas) {
+      System.out.printf("%15s |", elemento);
+    }
+    System.out.println();
+    //Ordenar las palabras
+    String[] palabrasOrdenadas = new String[N];
+    int numColores = 0, numNoColores = N - 1;
+    for (int i = 0; i < N; i++) {
+      boolean color = false;
+      switch (palabrasDesordenadas[i]) {
+        case "verde":
+        case "rojo":
+        case "azul":
+        case "amarillo":
+        case "naranja":
+        case "rosa":
+        case "negro":
+        case "blanco":
+        case "morado":
+          color = true;
+          break;
+        default:
+      }
+      if (color) {
+        palabrasOrdenadas[numColores] = palabrasDesordenadas[i];
+        numColores++;
+      } else {
+        palabrasOrdenadas[numNoColores] = palabrasDesordenadas[i];
+        numNoColores--;
+      }
+    }
+    System.out.println();
+    System.out.println("-----Palabras ordenadas-----");
+    //Mostrar las palabras ordenadas
+    System.out.print("Índices: ");
+    for (int i = 0; i < N; i++) {
+      System.out.printf("%15d |", i);
+    }
+    System.out.println();
+    System.out.print("Valores: ");
+    for (String elemento : palabrasOrdenadas) {
+      System.out.printf("%15s |", elemento);
+    }
+    System.out.println();
   }
 }
