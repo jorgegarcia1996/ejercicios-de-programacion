09e0895904098da8304cad9f6d7fccc9d8eba658
jorgegarcia1996 <jorgetrapiche1996@gmail.com>
2018-11-27 14:12:16 +0100

    Fichero con funciones de conversion

---
+++ Tema08/funciones/FuncionesDeConversion.java
@@ -0,0 +1,77 @@
+package funciones;
+
+/**
+ * Este archivo tiene funciones de conversión de los distintos sistemas numéricos
+ * (Bunario, Octal, Decimal y Hexadecimal)
+ * 
+ * @author Jorge García Molina
+ */
+public class FuncionesDeConversion {
+  
+  /**
+   * Funcion para pasar un número de Octal (O), decimal (D) o hexadecimal (H) a binario
+   * 
+   * @param n Es el número que se quiere transformar
+   * @param t es el tipo de sistema del número 'n'
+   * @return Devuelve el número 'n' convertido a binario
+   */
+  public static long convierteABinario(long n, String t) {
+    
+  }
+  
+  /**
+   * Funcion para pasar un número de Binario (B), decimal (D) o hexadecimal (H) a Octal
+   * 
+   * @param n Es el número que se quiere transformar
+   * @param t es el tipo de sistema del número 'n'
+   * @return Devuelve el número 'n' convertido a binario
+   */
+  public static long convierteAOctal(long n, String t) {
+    
+  }
+  
+  /**
+   * Funcion para pasar un número de Octal (O), decimal (D) o hexadecimal (H) a binario
+   * 
+   * @param n Es el número que se quiere transformar
+   * @param t es el tipo de sistema del número 'n'
+   * @return Devuelve el número 'n' convertido a binario
+   */
+  public static long convierteABinario(long n, String t) {
+    
+  }
+  
+  /**
+   * Funcion para pasar un número de Octal (O), decimal (D) o hexadecimal (H) a binario
+   * 
+   * @param n Es el número que se quiere transformar
+   * @param t es el tipo de sistema del número 'n'
+   * @return Devuelve el número 'n' convertido a binario
+   */
+  public static long convierteABinario(long n, String t) {
+    
+  }
+  
+  /**
+   * Funcion para convertir un número binario (B), decimal (D), octal (O) o hexadecimal (H) a
+   * otro sistema de esos cuatro
+   * 
+   * @param n es el número que se quiere convertir
+   * @param t1 Es el tipo de sistema al que pertenece el número introducido (B-O-D-H)
+   * @param t2 Es el sistema al que se quiere convertir el número (B-O-D-H)
+   * @return Devuelve un el número 'n' convertido al sistema 't2'
+   */
+  public static long conversionNumero(long n, String t1, String t2) {
+    switch (t2) {
+      case "B":
+        break;
+      case "O":
+        break;
+      case "D":
+        break;
+      case "H":
+        break;
+      default:
+    }
+  }
+}
