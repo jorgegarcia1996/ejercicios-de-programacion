144e9be516e60c71899a660d1d86cecdb8eede9a
jorgegarcia1996 <jorgetrapiche1996@gmail.com>
2019-01-23 11:11:54 +0100

    Ejercicio 3 comenzado

---
+++ Tema09/Parte3/Ejercicio03/Disco.java
@@ -0,0 +1,69 @@
+package parte3.Ejercicio03;
+
+/**
+ * Definición de la clase Disco
+ * 
+ * @author Luis José Sánchez
+ */
+
+public class Disco {
+  //Atributos
+  private String codigo = "LIBRE";
+  private String autor;
+  private String titulo;
+  private String genero;
+  private int duracion; // duración total en minutos
+  
+  
+  //Getters
+  public String getCodigo() {
+    return codigo;
+  }
+
+  public String getAutor() {
+    return autor;
+  }
+
+  public String getTitulo() {
+    return titulo;
+  }
+
+  public String getGenero() {
+    return genero;
+  }
+
+  public int getDuracion() {
+    return duracion;
+  }
+  
+  //Setters
+  public void setCodigo(String codigo) {
+        this.codigo = codigo;
+  }
+
+  public void setAutor(String autor) {
+    this.autor = autor;
+  }
+
+  public void setTitulo(String titulo) {
+    this.titulo = titulo;
+  }
+
+  public void setGenero(String genero) {
+    this.genero = genero;
+  }
+
+  public void setDuracion(int duracion) {
+    this.duracion = duracion;
+  }
+
+  //Métodos
+  @Override
+  public String toString() {
+    return "\n------------------------------------------"
+    + "\nCódigo: " + this.codigo + "\nAutor: " + this.autor
+    + "\nTítulo: " + this.titulo + "\nGénero: " + this.genero
+    + "\nDuración: " + this.duracion 
+    + "\n------------------------------------------";
+  }
+}
---
+++ Tema09/Parte3/Ejercicio03/Ejercicio03.java
@@ -0,0 +1,16 @@
+package parte3.Ejercicio03;
+
+/**
+ * Ejercicio 03 del Tema 09 (Parte 3)
+ * 
+ * Realiza el programa “Colección de discos” por tu cuenta, mirando lo menos
+ * posible el ejemplo que se proporciona. Pruébalo primero para ver cómo
+ * funciona y luego intenta implementarlo tú mismo.
+ *
+ * @author JorgeGarciaMolina
+ */
+public class Ejercicio03 {
+  public static void main(String[] args) {
+    
+  }
+}
